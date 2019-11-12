3dc458a5ee1cc17cd9ceb9bcfee88a394a76a9b7
ijm51000 <ianmac51@gmail.com>
2019-11-12 12:02:27 +0000

    Add random fortune method

--- Spring-demo-one/src/com/luv2code/springdemo/HappyFortuneService.java
+++ Spring-demo-one/src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,9 +1,21 @@
 package com.luv2code.springdemo;
+import java.util.Random;
 
 public class HappyFortuneService implements FortuneService {
+	private String[] data = {
+			"don't eat yellow snow",
+			"luck is driven not given",
+			"fortune favours the bold"
+	};
+	// create a random number generator
+	private Random myRandom = new Random();
+
 	@Override
 	public String getFortune() {
-		return "Today is your lucky day!";
+		// pick a random string from the array
+		int index = myRandom.nextInt(data.length);
+		String theFortune = data[index];
+		return theFortune;
 	}
 
 }
