13d510625f1e7b21342c79610115694e87da90a6
ijm51000 <ianmac51@gmail.com>
2019-11-12 11:46:29 +0000

    add properties file

--- Spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
+++ Spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
@@ -3,7 +3,31 @@ package com.luv2code.springdemo;
 public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService;
+	// Injecting literal values 
+	// Add new fields 
+	private String emailAddress;
+	private String team;
+
 	
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: Inside the setter method setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: Inside the setter method setTeam");
+		this.team = team;
+	}
+
 	// create a no-arg constructor
 	public CricketCoach() {
 		System.out.println("CricketCoach: Inside the no-arg constructor");
--- Spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
+++ Spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
@@ -12,10 +12,13 @@ public class SetterDemoApp {
 		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
-		
 		System.out.println(theCoach.getDailyFortune());
-		// close the context
 		
+		// call new methods to get literal values
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
+		
+		// close the context
 		context.close();
 	}
 
